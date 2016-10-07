601dd951e303ac6f4c0071422790bd024b7051c9
harshilbshah <harshilbshah2015@gmail.com>
2016-10-07 21:06:55 +0530

    Add files via upload

---
+++ Inverse_matrix_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:38:30 10/07/2016
+// Design Name:   Inverse_Matrix
+// Module Name:   F:/Xilinx Projects/Inverse_Matrix/Inverse_matrix_tb.v
+// Project Name:  Inverse_Matrix
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Inverse_Matrix
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Inverse_matrix_tb;
+
+	// Inputs
+	reg clk;
+
+	// Instantiate the Unit Under Test (UUT)
+	Inverse_Matrix uut (
+		.clk(clk)
+	);
+
+	always #50 clk = ~clk;
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
